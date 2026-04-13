8072da25ab0378692fde6ea6d32ca0b8e0913e41
smazumder <smazumder@qnx.com>
2026-04-13 18:26:04 -0400

    deleting rpi4 codelab

--- docs/codelabs-list.js
+++ docs/codelabs-list.js
@@ -18,30 +18,13 @@ window.CODELABS = [
     "href": "codelab-to-create-codelab/index.html",
     "id": "codelab-to-create-codelab",
     "authors": "Shweta Mazumder",
-    "updated": "2026-02-13T13:16:03-05:00",
+    "updated": "2026-04-13T18:19:14-04:00",
     "category": [
       "codelabs",
       "setup"
     ],
     "tags": [
       "beginner"
-    ]
-  },
-  {
-    "title": "Build your own QNX 8.0 Custom Target Image (CTI) for Raspberry Pi 4",
-    "href": "custom-target-image-rpi4/index.html",
-    "id": "custom-target-image-rpi4",
-    "authors": "QNX",
-    "updated": "2026-02-10T16:42:30-05:00",
-    "category": [
-      "qnx",
-      "embedded",
-      "raspberry-pi"
-    ],
-    "tags": [
-      "embedded-linux",
-      "qnx",
-      "rpi4"
     ]
   },
   {

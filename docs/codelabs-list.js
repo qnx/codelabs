c3f0cfac918d9944f88b2cf7ba00d0f632573ea5
John Hanam (QNX) <john@blackberry.com>
2026-04-30 20:36:01 -0400

    Update codelabs list with updated titles and summaries

--- docs/codelabs-list.js
+++ docs/codelabs-list.js
@@ -1,9 +1,10 @@
 window.CODELABS = [
   {
-    "title": "Building Maelstrom on QNX",
+    "title": "Build Maelstrom on QNX",
     "href": "build-maelstrom-on-qnx/index.html",
     "id": "build-maelstrom-on-qnx",
     "authors": "Roberto Speranza",
+    "summary": "Learn how to modify and build the game Maelstrom on QNX.",
     "updated": "2026-04-16T16:56:22-04:00",
     "category": [
       "self-hosting",
@@ -14,10 +15,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Use Claude Code on QNX Developer Desktop",
+    "title": "Use Claude Code on QNX Developer Desktop.",
     "href": "claude-code/index.html",
     "id": "claude-code",
     "authors": "QNX Developer Relations",
+    "summary": "Learn how to leverage Claude Code for target-aware AI development on QNX 8.",
     "updated": "2026-04-21T16:51:36-04:00",
     "category": [
       "ai"
@@ -27,10 +29,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Create Your Own Codelab and Contribute to QNX",
+    "title": "Create Your Own QNX Codelab",
     "href": "codelab-to-create-codelab/index.html",
     "id": "codelab-to-create-codelab",
     "authors": "Shweta Mazumder",
+    "summary": "Learn how to contribute to QNX codelabs.",
     "updated": "2026-04-16T17:08:12-04:00",
     "category": [
       "codelabs",
@@ -41,10 +44,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Building Standalone Epiphany Browser on QNX 8.0",
+    "title": "Build The Standalone Epiphany Browser on QNX 8.0",
     "href": "epiphany-standalone-qnx/index.html",
     "id": "epiphany-standalone-qnx",
     "authors": "Elliott Mazzuca",
+    "summary": "Run the Epiphany web browser on a minimal QNX 8.0 image.",
     "updated": "2026-04-16T17:08:34-04:00",
     "category": [
       "qnx",
@@ -61,6 +65,7 @@ window.CODELABS = [
     "href": "get-non-commercial-qnx-license/index.html",
     "id": "get-non-commercial-qnx-license",
     "authors": "Anna Likozar",
+    "summary": "Learn how to get a free QNX License.",
     "updated": "2026-04-16T17:15:18-04:00",
     "category": [
       "qnx",
@@ -71,10 +76,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Running Pong with Godot on QNX",
+    "title": "Build Pong With Godot on QNX",
     "href": "godot-codelab/index.html",
     "id": "godot-codelab",
     "authors": "Yun Lee",
+    "summary": "Build a Pong sample in Godot for QNX.",
     "updated": "2026-04-16T17:10:35-04:00",
     "category": [
       "qnx",
@@ -85,10 +91,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Running the Quick Start Target Image (QSTI) on QEMU using Windows Subsystem for Linux (WSL)",
+    "title": "Run the Quick Start Target Image (QSTI) on QEMU using Windows Subsystem for Linux (WSL)",
     "href": "qsti-qemu-on-wsl/index.html",
     "id": "qsti-qemu-on-wsl",
     "authors": "Anthony Sabbagh",
+    "summary": "Install prerequisites, configure, and run the QSTI on QEMU with WSL on Windows 11.",
     "updated": "2026-04-16T17:11:18-04:00",
     "category": [
       "qnx",
@@ -99,10 +106,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Using NVMe SSD on Raspberry Pi 5 using QNX",
+    "title": "Use NVMe SSDs in QNX on Raspberry Pi 5",
     "href": "rpi5-using-nvme-ssd/index.html",
     "id": "rpi5-using-nvme-ssd",
     "authors": "Karol Frackiewicz",
+    "summary": "Learn how to install the NVMe disk driver to access NVMe SSDs.",
     "updated": "2026-03-02T11:14:12-05:00",
     "category": [
       "qnx",
@@ -118,10 +126,11 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Running the Quick Start Target Image (QSTI) on QEMU using Ubuntu",
+    "title": "Run the Quick Start Target Image (QSTI) on QEMU using Ubuntu",
     "href": "setup-qsti-qemu-on-ubuntu/index.html",
     "id": "setup-qsti-qemu-on-ubuntu",
     "authors": "Anna Likozar",
+    "summary": "Learn how to set up the QSTI on QEMU and verify your compiler setup.",
     "updated": "2026-04-16T17:12:00-04:00",
     "category": [
       "qnx",

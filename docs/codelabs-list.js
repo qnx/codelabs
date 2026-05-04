0ed43af6d06fd98697736e5628fd7985f8333a8b
Yun Lee <yulee@blackberry.com>
2026-05-04 16:14:03 -0400

    Yun codelab addition #33 #34 and update landing page

--- docs/codelabs-list.js
+++ docs/codelabs-list.js
@@ -1,11 +1,11 @@
 window.CODELABS = [
   {
-    "title": "Build Maelstrom on QNX",
+    "title": "Building Maelstrom on QNX",
     "href": "build-maelstrom-on-qnx/index.html",
     "id": "build-maelstrom-on-qnx",
     "authors": "Roberto Speranza",
-    "summary": "Learn how to modify and build the game Maelstrom on QNX.",
-    "updated": "2026-04-16T16:56:22-04:00",
+    "summary": "Learn how to modify and build the game Maelstrom on QNX",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "self-hosting",
       "sdlgames"
@@ -15,12 +15,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Use Claude Code on QNX Developer Desktop.",
+    "title": "Use Claude Code on QNX Developer Desktop",
     "href": "claude-code/index.html",
     "id": "claude-code",
     "authors": "QNX Developer Relations",
     "summary": "Learn how to leverage Claude Code for target-aware AI development on QNX 8.",
-    "updated": "2026-04-21T16:51:36-04:00",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "ai"
     ],
@@ -29,12 +29,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Create Your Own QNX Codelab",
+    "title": "Create Your Own Codelab and Contribute to QNX",
     "href": "codelab-to-create-codelab/index.html",
     "id": "codelab-to-create-codelab",
     "authors": "Shweta Mazumder",
-    "summary": "Learn how to contribute to QNX codelabs.",
-    "updated": "2026-04-16T17:08:12-04:00",
+    "summary": "Learn how to contribute to QNX Codelabs",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "codelabs",
       "setup"
@@ -44,12 +44,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Build The Standalone Epiphany Browser on QNX 8.0",
+    "title": "Building Standalone Epiphany Browser on QNX 8.0",
     "href": "epiphany-standalone-qnx/index.html",
     "id": "epiphany-standalone-qnx",
     "authors": "Elliott Mazzuca",
-    "summary": "Run the Epiphany web browser on a minimal QNX 8.0 image.",
-    "updated": "2026-04-16T17:08:34-04:00",
+    "summary": "Running the Epiphany web browser on a minimal QNX 8.0 image without the full desktop stack",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "epiphany",
@@ -65,8 +65,8 @@ window.CODELABS = [
     "href": "get-non-commercial-qnx-license/index.html",
     "id": "get-non-commercial-qnx-license",
     "authors": "Anna Likozar",
-    "summary": "Learn how to get a free QNX License.",
-    "updated": "2026-04-16T17:15:18-04:00",
+    "summary": "Learn how to get a non-commercial QNX License for use with QNXe",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "setup"
@@ -76,12 +76,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Build Pong With Godot on QNX",
+    "title": "Running Pong with Godot on QNX",
     "href": "godot-codelab/index.html",
     "id": "godot-codelab",
     "authors": "Yun Lee",
-    "summary": "Build a Pong sample in Godot for QNX.",
-    "updated": "2026-04-16T17:10:35-04:00",
+    "summary": "Running Pong with Godot on QNX",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "godot"
@@ -91,12 +91,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Run the Quick Start Target Image (QSTI) on QEMU using Windows Subsystem for Linux (WSL)",
+    "title": "Running the Quick Start Target Image (QSTI) on QEMU using Windows Subsystem for Linux (WSL)",
     "href": "qsti-qemu-on-wsl/index.html",
     "id": "qsti-qemu-on-wsl",
     "authors": "Anthony Sabbagh",
-    "summary": "Install prerequisites, configure, and run the QSTI on QEMU with WSL on Windows 11.",
-    "updated": "2026-04-16T17:11:18-04:00",
+    "summary": "Install prerequisites, configure, and run QSTI on QEMU with WSL on Windows 11.",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "windows"
@@ -106,12 +106,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Use NVMe SSDs in QNX on Raspberry Pi 5",
+    "title": "Using NVMe SSD on Raspberry Pi 5 using QNX",
     "href": "rpi5-using-nvme-ssd/index.html",
     "id": "rpi5-using-nvme-ssd",
     "authors": "Karol Frackiewicz",
-    "summary": "Learn how to install the NVMe disk driver to access NVMe SSDs.",
-    "updated": "2026-03-02T11:14:12-05:00",
+    "summary": "Learn how to install the NVMe disk driver to access NVMe SSD.",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "embedded",
@@ -126,12 +126,12 @@ window.CODELABS = [
     ]
   },
   {
-    "title": "Run the Quick Start Target Image (QSTI) on QEMU using Ubuntu",
+    "title": "Running the Quick Start Target Image (QSTI) on QEMU using Ubuntu",
     "href": "setup-qsti-qemu-on-ubuntu/index.html",
     "id": "setup-qsti-qemu-on-ubuntu",
     "authors": "Anna Likozar",
-    "summary": "Learn how to set up the QSTI on QEMU and verify your compiler setup.",
-    "updated": "2026-04-16T17:12:00-04:00",
+    "summary": "Learn how to setup QSTI on QEMU and verify compiler setup",
+    "updated": "2026-04-30T14:58:04-04:00",
     "category": [
       "qnx",
       "qemu"
